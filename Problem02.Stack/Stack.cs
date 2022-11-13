da698b8319f503791ca2a354f456246d7a53e806
BlagoKolev <blagovest.kolev@gmail.com>
2022-11-13 23:21:16 +0200

    Implement Stack.

--- Problem02.Stack/Stack.cs
+++ Problem02.Stack/Stack.cs
@@ -6,43 +6,79 @@
 
     public class Stack<T> : IAbstractStack<T>
     {
-        private class Node
+        private class Node<T>
         {
-            
+            public Node(T element)
+            {
+                this.Item = element;
+            }
+            public T Item { get; set; }
+            public Node<T> Next { get; set; }
         }
 
-        private Node top;
+        private Node<T> top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item);
+            newNode.Next = this.top;
+            this.top = newNode;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            CheckIsStackEmpty();
+            var elementToPop = this.top;
+            this.top = this.top.Next;
+            this.Count--;
+            return elementToPop.Item;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            CheckIsStackEmpty();
+            return this.top.Item;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            var current = this.top;
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.top;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private void CheckIsStackEmpty()
+        {
+            if (this.top == null)
+            {
+                throw new InvalidOperationException("Unable to perform this operation. The Stack is empty");
+            }
         }
     }
 }
