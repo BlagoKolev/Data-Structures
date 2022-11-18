7439cf616eb4dae5ec4e1d3b9d8ed1b43439c437
BlagoKolev <blagovest.kolev@gmail.com>
2022-11-18 14:09:16 +0200

    Implement Queue.

--- Problem03.Queue/Queue.cs
+++ Problem03.Queue/Queue.cs
@@ -8,41 +8,85 @@
     {
         private class Node
         {
-            
+            public Node(T item)
+            {
+                this.Item = item;
+            }
+            public T Item { get; set; }
+            public Node Next { get; set; }
         }
 
         private Node head;
+        private Node tail;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = this.head;
+            }
+            else
+            {
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+            this.Count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            CheckForEmptyCollection();
+            var elementToDequeue = this.head;
+            this.head = this.head.Next;
+            Count--;
+            return elementToDequeue.Item;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            CheckForEmptyCollection();
+            return this.head.Item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var current = this.head;
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.head;
+
+            while (current != null)
+            {
+                yield return this.head.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+        private void CheckForEmptyCollection()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Can not perform operation on empty collection.");
+            }
         }
     }
 }
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -2,9 +2,32 @@
 using Problem01.List;
 
 
-var list = new Problem01.List.List<int>() ;
-list.Add(1);
-list.Add(2);
-list.Add(3);
-list.Insert(2, 199);
-Console.WriteLine("Hello, World!");
+
+
+var queue = new Problem03.Queue.Queue<int>();
+var defaultQueue = new System.Collections.Generic.Queue<int>();
+queue.Enqueue(1);   
+defaultQueue.Enqueue(1);
+
+queue.Enqueue(2);
+defaultQueue.Enqueue(2);
+
+queue.Enqueue(3);
+defaultQueue.Enqueue(3);
+
+queue.Enqueue(4);
+defaultQueue.Enqueue(4);
+
+queue.Enqueue(5);
+defaultQueue.Enqueue(5);
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(queue.Dequeue());
+    Console.WriteLine(defaultQueue.Dequeue());
+}
+
+
+
+
