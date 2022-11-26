9324f341efadc374e00eb655c8650d1bcef44467
BlagoKolev <blagovest.kolev@gmail.com>
2022-11-26 09:39:33 +0200

    Implement Doubly Linked List.

--- More-Linear-Data-Structures/02.DoublyLinkedList/DoublyLinkedList.cs
+++ More-Linear-Data-Structures/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -6,46 +6,128 @@
 
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
+        private Node<T> head;
+        private Node<T> tail;
+        private class Node<T>
+        {
+            public Node<T> Previous { get; set; }
+            public Node<T> Next { get; set; }
+            public T Item { get; set; }
+        }
+
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>();
+
+            if (this.Count == 0)
+            {
+                this.tail = this.head = newNode;
+            }
+            else
+            {
+                this.head.Previous = newNode;
+                newNode.Next = this.head;
+                this.head = newNode;
+            }
+            //else if (this.Count == 1)
+            //{
+            //    this.head = newNode;
+            //    this.head.Next = this.tail;
+            //    this.tail.Previous = this.head;
+            //}
+            //else
+            //{
+            //    var previousHead = this.head;
+            //    this.head = newNode;
+            //    this.head.Next = previousHead;
+            //    previousHead.Previous = this.head;
+
+            //}
+
+            this.head.Item = item;
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>();
+            if (this.Count == 0)
+            {
+                this.head = this.tail = newNode;
+            }
+            else
+            {
+                this.tail.Next = newNode;
+                newNode.Previous = this.tail;
+                this.tail = newNode;
+            }
+            this.tail.Item = item;
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            CheckIsEmpty();
+            return this.head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            CheckIsEmpty();
+            return this.tail.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            CheckIsEmpty();
+            var itemToReturn = this.head.Item;
+            this.head = this.head.Next;
+            this.Count--;
+            return itemToReturn;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            CheckIsEmpty();
+            var itemToReturn = this.tail;
+
+            if (itemToReturn.Previous == null)
+            {
+                this.tail = this.head = null;
+            }
+            else
+            {
+                this.tail = this.tail.Previous;
+                this.tail.Next = null;
+            }
+           
+            this.Count--;
+            return itemToReturn.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private void CheckIsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The Collection is empty");
+            }
         }
     }
 }
---
+++ More-Linear-Data-Structures/SandBox/Program.cs
@@ -0,0 +1,24 @@
+﻿using Problem02.DoublyLinkedList;
+using System;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doubleList = new DoublyLinkedList<int>();
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var item in arr)
+            {
+                doubleList.AddLast(item);
+                Console.WriteLine(doubleList.GetLast());
+            }
+            for (int i = 0; i < arr.Length+1; i++)
+            {
+                var a = doubleList.RemoveLast();
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
